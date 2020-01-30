0a9f2fcb4be6996c5e506066421c509c80981c4b
m5kR10bHxS <dlwhite67gmail.com>
2020-01-30 00:16:02 -0600

    Base layer toggle fixed

--- keyboards/alps64/keymaps/dw/keymap.c
+++ keyboards/alps64/keymaps/dw/keymap.c
@@ -19,10 +19,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT_infinity( \
       KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSLS, KC_GRV, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSPC, \
-      KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TG(WORKMAN), \
-      KC_LCTL, KC_LGUI, KC_LALT,                  KC_SPC,                                     KC_RALT, KC_RGUI, MO(LOWER), KC_RCTL \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSPC,         \
+      KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TG(WORKMAN),               \
+      KC_LCTL, KC_LGUI, KC_LALT,                  KC_SPC,                                     KC_RALT, KC_RGUI, MO(LOWER), KC_RCTL         \
   ),
 
 /*
@@ -44,26 +44,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [WORKMAN] = LAYOUT_infinity( \
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSLS, KC_GRV, \
-      KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,   KC_F,    KC_U,    KC_P,    KC_SCLN, KC_LBRC, KC_RBRC,  KC_BSPC, \
-      KC_BSPC, KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,   KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, KC_ENT, \
-      KC_LCTL, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,   KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL, TG(QWERTY), \
-      KC_CCCV, KC_LGUI, KC_LALT,                   LT(NAV,KC_SPC),                             KC_RALT, KC_RGUI, MO(LOWER), KC_LEAD \
+      KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,   KC_F,    KC_U,    KC_P,    KC_SCLN, KC_LBRC, KC_RBRC,  KC_BSPC,         \
+      KC_BSPC, KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,   KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, KC_ENT,                    \
+      KC_LCTL, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,   KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL, _______,                   \
+      KC_CCCV, KC_LGUI, KC_LALT,                   LT(NAV,KC_SPC),                             KC_RALT, KC_RGUI, MO(LOWER), KC_LEAD         \
   ),
 
   [LOWER] = LAYOUT_infinity( \
       _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12, _______,  _______, \
-      _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-      KC_ASON,  KC_ASUP, KC_ASRP, _______, KC_VOLU, KC_MPLY, _______, KC_KEEB, KC_TMRK, _______, _______, _______, _______, \
-      KC_ASOFF, KC_ASDN, _______, _______, KC_VOLD, _______, _______, _______, _______, _______, _______, _______, _______, \
-      _______,  _______, _______,                   _______,                                     _______, _______, _______, _______ \
+      _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           \
+      KC_ASON,  KC_ASUP, KC_ASRP, _______, KC_VOLU, KC_MPLY, _______, KC_KEEB, KC_TMRK, _______, _______, _______, _______,                    \
+      KC_ASOFF, KC_ASDN, _______, _______, KC_VOLD, _______, _______, _______, _______, _______, _______, _______, _______,                    \
+      _______,  _______, _______,                   _______,                                     _______, _______, _______, _______            \
   ),
 
   [NAV] = LAYOUT_infinity( \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______, _______,  _______, _______, \
-      _______, _______, _______,                   _______,                                     _______,  _______, _______, _______ \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,          \
+      _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,                   \
+      _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______, _______,  _______, _______,                   \
+      _______, _______, _______,                   _______,                                     _______,  _______, _______, _______           \
   ),
 };
 

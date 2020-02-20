8b3f8502a86cf023db4109b6daca5846f06deaad
m5kR10bHxS <dlwhite67@gmail.com>
2020-02-20 06:00:26 -0600

    Automatically turn on auto shift for _WORKMAN and off fer _QWERTY

--- keyboards/alps64/keymaps/dw/keymap.c
+++ keyboards/alps64/keymaps/dw/keymap.c
@@ -3,11 +3,11 @@
 uint16_t copy_paste_timer;
 
 enum layers {
-  QWERTY = 0,
-  WORKMAN,
-  LOWER,
-  NAV,
-  ADJUST
+  _QWERTY = 0,
+  _WORKMAN,
+  _LOWER,
+  _NAV,
+  _ADJUST
 };
 
 enum custom_keycodes {
@@ -35,12 +35,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  */
 
-  [QWERTY] = LAYOUT_infinity( \
-      KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSLS, KC_GRV, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSPC,         \
-      KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TG(WORKMAN),               \
-      KC_LCTL, KC_LGUI, KC_LALT,                  KC_SPC,                                     KC_RALT, KC_RGUI, MO(LOWER), KC_RCTL         \
+  [_QWERTY] = LAYOUT_infinity( \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSLS, KC_GRV, \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,    KC_BSPC,         \
+      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                      \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TG(_WORKMAN),                \
+      KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, KC_RGUI, MO(_LOWER), KC_RCTL          \
   ),
 
 /*
@@ -60,23 +60,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  */
 
-  [WORKMAN] = LAYOUT_infinity( \
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSLS, KC_GRV, \
-      KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,   KC_F,    KC_U,    KC_P,    KC_SCLN, KC_LBRC, KC_RBRC,  KC_BSPC,         \
-      KC_BSPC, KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,   KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, KC_ENT,                    \
-      KC_LCTL, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,   KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL, _______,                   \
-      KC_CCCV, KC_LGUI, KC_LALT,                   LT(NAV,KC_SPC),                             KC_RALT, KC_RGUI, MO(LOWER), KC_LEAD         \
+  [_WORKMAN] = LAYOUT_infinity( \
+      KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSLS, KC_GRV, \
+      KC_TAB,   KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,     KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN, KC_LBRC, KC_RBRC,    KC_BSPC,         \
+      KC_BSPC,  KC_A,    KC_S,    KC_H,    KC_T,    KC_G,     KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, KC_ENT,                      \
+      KC_LCTL,  KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,     KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL, _______,                     \
+      KC_CCCV,  KC_LGUI, KC_LALT,                   LT(_NAV,KC_SPC),                              KC_RALT, KC_RGUI, MO(_LOWER), KC_LEAD          \
   ),
 
-  [LOWER] = LAYOUT_infinity( \
-      _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12, _______,  _______, \
-      _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           \
-      KC_ASON,  KC_ASUP, KC_ASRP, _______, _______, _______, _______, KC_KEEB, KC_TMRK, _______, _______, _______, _______,                    \
-      KC_ASOFF, KC_ASDN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                    \
-      _______,  _______, _______,                   _______,                                     _______, _______, _______, _______            \
+  [_LOWER] = LAYOUT_infinity( \
+      _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  _______, \
+      _______,  _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______,           \
+      KC_ASON,  KC_ASUP, KC_ASRP, _______, _______, _______,  _______, KC_KEEB, KC_TMRK, _______, _______, _______, _______,                    \
+      KC_ASOFF, KC_ASDN, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,                    \
+      _______,  _______, _______,                   _______,                                      _______, _______, _______, _______            \
   ),
 
-  [NAV] = LAYOUT_infinity( \
+  [_NAV] = LAYOUT_infinity( \
       _______, _______, KC_INS,  KC_HOME, KC_PGUP, KC_SYSREQ, _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
       _______, _______, KC_DEL,  KC_END,  KC_PGDN, KC_PAUS,   _______, _______, _______, _______, _______,  _______, _______, _______,          \
       KC_CAPS, _______, _______, _______, _______, KC_BRK,    _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,                   \
@@ -84,17 +84,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______,                   _______,                                       _______,  _______, _______, _______           \
   ),
 
-[ADJUST] = LAYOUT_infinity( \
-      _______, _______, KC_MNXT, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
-      _______, _______, KC_MPRV, KC_VOLD, KC_MPLY, _______, _______, _______, _______, _______, _______,  _______, _______, _______,          \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,                   \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,                   \
-      RESET,   _______, _______,                   _______,                                     _______,  _______, _______, _______           \
+[_ADJUST] = LAYOUT_infinity( \
+      _______, _______, KC_MNXT, KC_VOLU, KC_MUTE, _______,   _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
+      _______, _______, KC_MPRV, KC_VOLD, KC_MPLY, _______,   _______, _______, _______, _______, _______,  _______, _______, _______,          \
+      _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______,  _______, _______,                   \
+      _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______,  _______, _______,                   \
+      RESET,   _______, _______,                   _______,                                       _______,  _______, _______, _______           \
   ),
 };
 
+// Change to _ADJUST layer, if both _LOWER and _NAV layers are held
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, LOWER, NAV, ADJUST);
+    return update_tri_layer_state(state, _LOWER, _NAV, _ADJUST);
+}
+
+// Automate the auto shift feature: off w/ _QWERTY, but on w/ _WORKMAN
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    if (layer_state_cmp(state, _WORKMAN)) {
+        autoshift_enable();
+    } else {
+        autoshift_disable();
+    }
+    return state;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {

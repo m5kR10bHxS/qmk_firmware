0a55e73cbc58c6625612843b847af998b88ef410
m5kR10bHxS <dlwhite67@gmail.com>
2020-02-22 09:12:12 -0600

    Relocate CTRL keys, reformat layer definitions

--- keyboards/alps64/keymaps/dw/keymap.c
+++ keyboards/alps64/keymaps/dw/keymap.c
@@ -1,16 +1,29 @@
 // Alps64
 
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include QMK_KEYBOARD_H
 
 uint16_t copy_paste_timer;
 
-enum layers {
-  _QWERTY = 0,
-  _WORKMAN,
-  _LOWER,
-  _NAV,
-  _ADJUST
-};
+#define _QWERTY  0
+#define _WORKMAN 1
+#define _LOWER   2
+#define _NAV     3
+#define _ADJUST  4
 
 enum custom_keycodes {
     KC_CCCV = SAFE_RANGE,
@@ -20,8 +33,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/*
- * Base Layer: QWERTY
+/* Base Layer: QWERTY
  *
  *  --------+------+------+------+------+------+------+------+------+------+------+-------+-------+------+--------        
  * |  Esc   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  - _  |  = +  |  | \ |  ` ~   |
@@ -32,9 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+------+------+------+------+------+-------+-------+---------------|
  * |     Shift     |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |  , < |  . > |  /  ? |    Shift      |WORKMAN|
  * |--------+------+------+------+------+------+------+------+------+------+------+-------+-------+-------+-------|
- * |  Ctrl  | Menu |  Alt |                         Space                         |  Alt  |  Menu | LOWER |  Ctrl |
+ * |  Ctrl  | GUI  |  Alt |                         Space                         |  Alt  |  GUI  | LOWER |  Ctrl |
  *  --------+------+------+-------------------------------------------------------+-------+-------+-------+-------'
- *
  */
 
   [_QWERTY] = LAYOUT_infinity( \
@@ -45,53 +56,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, KC_RGUI, MO(_LOWER), KC_RCTL          \
   ),
 
-/*
- * Base Layer: WORKMAN
+/* Base Layer: WORKMAN
  *
  *  --------+------+------+------+------+------+------+------+------+------+------+-------+-------+------+--------        
  * |  Esc   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  - _  |  = +  |  | \ |  ` ~   |
  * |--------+------+------+------+------+------+------+------+------+------+------+-------+-------+------+--------|
  * |  Tab   |   Q  |   D  |   R  |   W  |   B  |   J  |   F  |   U  |   P  | ;  : |  [ {  |  ] }  |    Bk Spc     |
  * |--------+------+------+------+------+------+------+------+------+------+------+-------+-------+------+--------|
- * |     Bk Spc    |   A  |   S  |  H   |   T  |   G  |   Y  |   N  |   E  |   O  |   I   |  ' "  |    Enter      |
+ * |      Ctrl     |   A  |   S  |  H   |   T  |   G  |   Y  |   N  |   E  |   O  |   I   |  ' "  |    Enter      |
  * |--------+------+------+------+------+------+------+------+------+------+------+-------+-------+---------------|
- * |      Ctrl     |   Z  |   X  |   M  |   C  |   V  |   K  |   L  |  , < |  . > |  /  ? |     Ctrl      | QWERTY|
+ * |      Shift    |   Z  |   X  |   M  |   C  |   V  |   K  |   L  |  , < |  . > |  /  ? |     Shift     | QWERTY|
  * |--------+------+------+------+------+------+------+------+------+------+------+-------+-------+-------+-------|
- * |  CCCV  | Menu |  Alt |                     Space Fn(NAV)                     |  Alt  |  Menu | LOWER | LEADER|
+ * |  CCCV  | Menu |  Alt |                     Space Fn(NAV)                     |  Alt  | LEADER| LOWER |  Ctrl |
  *  --------+------+------+-------------------------------------------------------+-------+-------+-------+-------'
- *
  */
 
   [_WORKMAN] = LAYOUT_infinity( \
       KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSLS, KC_GRV, \
       KC_TAB,   KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,     KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN, KC_LBRC, KC_RBRC,    KC_BSPC,         \
-      KC_BSPC,  KC_A,    KC_S,    KC_H,    KC_T,    KC_G,     KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, KC_ENT,                      \
-      KC_LCTL,  KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,     KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL, _______,                     \
-      KC_CCCV,  KC_LGUI, KC_LALT,                   LT(_NAV,KC_SPC),                              KC_RALT, KC_RGUI, MO(_LOWER), KC_LEAD          \
+      KC_LCTL,  KC_A,    KC_S,    KC_H,    KC_T,    KC_G,     KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, KC_ENT,                      \
+      KC_LSFT,  KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,     KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______,                     \
+      KC_CCCV,  KC_LGUI, KC_LALT,                   LT(_NAV,KC_SPC),                              KC_RALT, KC_LEAD, MO(_LOWER), KC_RCTL          \
   ),
 
   [_LOWER] = LAYOUT_infinity( \
       _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  _______, \
       _______,  _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______,           \
-      KC_ASON,  KC_ASUP, KC_ASRP, _______, _______, _______,  _______, KC_KEEB, KC_TMRK, _______, _______, _______, _______,                    \
-      KC_ASOFF, KC_ASDN, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,                    \
+      _______,  _______, _______, _______, _______, _______,  _______, KC_KEEB, KC_TMRK, _______, _______, _______, _______,                    \
+      _______,  _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,                    \
       _______,  _______, _______,                   _______,                                      _______, _______, _______, _______            \
   ),
 
   [_NAV] = LAYOUT_infinity( \
-      _______, _______, KC_INS,  KC_HOME, KC_PGUP, KC_SYSREQ, _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
-      _______, _______, KC_DEL,  KC_END,  KC_PGDN, KC_PAUS,   _______, _______, _______, _______, _______,  _______, _______, _______,          \
-      KC_CAPS, _______, _______, _______, _______, KC_BRK,    _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,                   \
-      _______, _______, _______, _______, _______, _______,   _______, KC_PGUP, KC_PGDN, _______, _______,  _______, _______,                   \
-      _______, _______, _______,                   _______,                                       _______,  _______, _______, _______           \
+      _______,  _______, KC_INS,  KC_HOME, KC_PGUP, KC_SYSREQ, _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
+      _______,  _______, KC_DEL,  KC_END,  KC_PGDN, KC_PAUS,   _______, _______, _______, _______, _______,  _______, _______, _______,          \
+      KC_CAPS,  _______, _______, _______, _______, KC_BRK,    _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,                   \
+      _______,  _______, _______, _______, _______, _______,   _______, KC_PGUP, KC_PGDN, _______, _______,  _______, _______,                   \
+      _______,  _______, _______,                   _______,                                       _______,  _______, _______, _______           \
   ),
 
 [_ADJUST] = LAYOUT_infinity( \
-      _______, _______, KC_MNXT, KC_VOLU, KC_MUTE, _______,   _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
-      _______, _______, KC_MPRV, KC_VOLD, KC_MPLY, _______,   _______, _______, _______, _______, _______,  _______, _______, _______,          \
-      _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______,  _______, _______,                   \
-      _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______,  _______, _______,                   \
-      RESET,   _______, _______,                   _______,                                       _______,  _______, _______, _______           \
+      _______,  _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______,  _______, _______, _______, _______, \
+      _______,  _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______,  _______, _______, _______,          \
+      KC_ASON,  KC_ASUP, KC_ASRP, _______, _______, _______,   _______, _______, _______, _______, _______,  _______, _______,                   \
+      KC_ASOFF, KC_ASDN, _______, _______, _______, _______,   _______, _______, _______, _______, _______,  _______, _______,                   \
+      RESET,    _______, _______,                   _______,                                       _______,  _______, _______, _______           \
   ),
 };
 
